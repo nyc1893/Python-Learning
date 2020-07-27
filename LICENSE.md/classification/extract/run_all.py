3512ad68d2b87d0f6400b13decc72143c4442a08
nyc1893 <nyc1893@163.com>
2020-07-27 11:13:21 -0700

    Update run_all.py

--- LICENSE.md/classification/extract/run_all.py
+++ LICENSE.md/classification/extract/run_all.py
@@ -1,12 +1,22 @@
 import os
 import timeit
-
-list = range(1,9) 
+# parameter 
+import subprocess
+# 1st: iteration times of Neat
+# 2rd: iteration times of how many runs
+# 3nd: lag numbers only 
 
 s1 = timeit.default_timer()  
 
+list = range(16,32) 
 for i in range(1,len(list)):
-    os.system('python ./dy.py 4 '+str(list[i-1])+' '+ str(list[i]))
-    
+    out = subprocess.call('python ./dy2.py 11 '+str(list[i-1])+' '+ str(list[i]), shell=True)
 s2 = timeit.default_timer()  
+
+list = range(1,14) 
+for i in range(1,len(list)):
+    out = subprocess.call('python ./dy2.py 12 '+str(list[i-1])+' '+ str(list[i]), shell=True)
+s2 = timeit.default_timer()  
+
+
 print ('Runing time is Hour:',round((s2 -s1)/3600,2))
