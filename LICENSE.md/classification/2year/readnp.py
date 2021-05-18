0c24d44894559d1ba47f8ee8f627d2c340df7803
nyc1893 <nyc1893@163.com>
2021-05-18 00:23:08 -0700

    Add files via upload

---
+++ LICENSE.md/classification/2year/readnp.py
@@ -0,0 +1,24 @@
+import numpy as np
+import math
+import pandas as pd
+
+
+
+arr = np.load("val_12.npy").tolist()
+print(arr)
+if (86 and 85 not in arr):
+    arr.append(85)
+    arr.append(86)
+arr = np.array(arr)
+np.save("val_12.npy", arr)
+
+list =[]
+arr = np.load("tr_12.npy").tolist()
+print(arr)
+for i in range(len(arr)): 
+    if(arr[i]!= 86 or arr[i]!= 85 ):
+        list.append(arr[i])
+        
+arr = np.array(list)
+np.save("tr_12.npy", arr)        
+    
