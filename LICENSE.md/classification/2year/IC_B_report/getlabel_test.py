2e923379a0c492b26b7805a80ea6e66e92b6f206
nyc1893 <nyc1893@163.com>
2022-02-13 22:18:53 -0800

    Add files via upload

---
+++ LICENSE.md/classification/2year/IC_B_report/getlabel_test.py
@@ -0,0 +1,230 @@
+
+
+# import matplotlib.pyplot as plt
+# import tensorflow as tf
+import numpy as np
+import math
+import pandas as pd
+import time  
+from sklearn import metrics  
+import pickle as pickle  
+import pandas as pd
+
+
+import pickle
+import datapick2
+# import datapick3
+import datetime
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
+# python getlabel2.py 2>&1 | tee b2.log
+# cat /proc/cpuinfo 2>&1 | tee b2.log
+
+from sklearn import datasets
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
+# from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+import time  
+from sklearn import metrics  
+import pickle as pickle  
+
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV, train_test_split
+import timeit
+import time  
+from sklearn import metrics  
+import pickle as pickle  
+import pandas as pd
+# from pandas._testing import assert_frame_equal
+
+# from rno_fun import data_lowfreq
+
+    
+    
+
+# KNN Classifier  
+def knn_classifier(train_x, train_y):  
+    from sklearn.neighbors import KNeighborsClassifier  
+    model = KNeighborsClassifier(n_neighbors = 17,leaf_size = 1,p = 1,n_jobs = 40)
+    model.fit(train_x, train_y)  
+    return model  
+  
+  
+# Logistic Regression Classifier  
+def logistic_regression_classifier(train_x, train_y):  
+    from sklearn.linear_model import LogisticRegression  
+    model = LogisticRegression(C=1.0, max_iter= 500, solver ='newton-cg',n_jobs=40)
+    model.fit(train_x, train_y)  
+    return model  
+  
+  
+# Random Forest Classifier  
+def random_forest_classifier(train_x, train_y):  
+    from sklearn.ensemble import RandomForestClassifier  
+    model = RandomForestClassifier(n_estimators=100)  
+    model.fit(train_x, train_y)  
+    return model  
+  
+  
+# Decision Tree Classifier  
+def decision_tree_classifier(train_x, train_y):  
+    from sklearn import tree  
+    model = tree.DecisionTreeClassifier()  
+    model.fit(train_x, train_y)  
+    return model  
+  
+  
+# GBDT(Gradient Boosting Decision Tree) Classifier  
+def gradient_boosting_classifier(train_x, train_y):  
+    from sklearn.ensemble import GradientBoostingClassifier  
+    model = GradientBoostingClassifier()
+    # model = GradientBoostingClassifier(learning_rate=0.01, n_estimators=1500,max_depth=7, min_samples_split=2, min_samples_leaf=1, subsample=1,max_features='sqrt', random_state=10) 
+    model.fit(train_x, train_y)  
+    return model  
+  
+  
+# SVM Classifier  
+def svm_classifier(train_x, train_y):  
+    from sklearn.svm import SVC  
+    model = SVC(kernel='rbf', probability=True)  
+    model.fit(train_x, train_y)  
+    return model  
+
+def svm_cross_validation(train_x, train_y):    
+    # from sklearn.grid_search import GridSearchCV    
+    from sklearn.svm import SVC    
+    model = SVC(kernel='rbf',  probability=True)    
+    param_grid = {'C': [1e-3, 1e-2, 1e-1, 1, 10, 100, 1000], 'gamma': [0.1,0.01,0.001, 0.0001]}    
+    grid_search = GridSearchCV(model, param_grid, n_jobs = 40, verbose=1)    
+    grid_search.fit(train_x, train_y)    
+    best_parameters = grid_search.best_estimator_.get_params()    
+    for para, val in list(best_parameters.items()):    
+        print(para, val)    
+    model = SVC(kernel= 'rbf', C=best_parameters['C'], gamma=best_parameters['gamma'], probability=True)    
+    model.fit(train_x, train_y)    
+    return model
+
+# SVM Classifier  
+def estimate():  
+    
+    
+    train_x, train_y,test_x, test_y =datapick2.top()  
+    print(train_x.shape) 
+    model = RandomForestClassifier(n_estimators=100)  
+    lsvc = model.fit(train_x,train_y)
+    model = SelectFromModel(lsvc, prefit=True)
+    X_new = model.transform(train_x)
+    print(X_new.shape)  
+
+
+      
+def run():
+    # data_file = "H:\\Research\\data\\trainCG.csv"  
+    s1 = timeit.default_timer()  
+
+    train_x, train_y =datapick2.train_eve_top()   
+    
+    test_x = datapick2.eve_test() 
+    # test_x.to_csv("test_x.csv",index = 0)
+    		
+
+    a1 = test_x.pop("f_name").values
+    a2 = test_x.pop("line_index").values
+    a3 = test_x.pop("freq_index").values
+
+    
+    X_train = train_x
+    X_test = test_x
+    
+    # y_test = test_y
+    y_train = train_y
+ 
+
+    train = 1
+    if train == 1:
+        thresh = 0.5  
+        model_save_file = None  
+        model_save = {}  
+       
+        test_classifiers = [
+
+             # 'KNN', 
+             # 'LR', 
+            'RF', 
+            # 'DT', 
+            # 'SVM',
+
+            'GBDT'
+            ]  
+            
+            
+        classifiers = {
+        
+                    # 'NB':naive_bayes_classifier,   
+                      # 'KNN':knn_classifier,  
+                       # 'LR':logistic_regression_classifier,  
+                       'RF':random_forest_classifier,  
+                       # 'DT':decision_tree_classifier,  
+                      # 'SVM':svm_classifier, 
+                       'SVM': svm_cross_validation,
+                    # 'SVMCV':svm_cross_validation,  
+                     'GBDT':gradient_boosting_classifier  
+        }  
+          
+        # print('reading training and testing data...')  
+        # 
+        # print('train_y.shape',y_test.shape)
+        tr = pd.DataFrame(y_train)
+        y_test = np.zeros(X_test.shape[0])
+        
+        te = pd.DataFrame(y_test)
+        te.columns = ['flag']
+        df  = y_test[:,np.newaxis]
+        # df2 = y_train[:,np.newaxis]
+        
+
+        
+
+        for classifier in test_classifiers:  
+            # print('******************* %s ********************' % classifier)  
+            start_time = time.time()  
+            model = classifiers[classifier](X_train, y_train)  
+            # print('training took %fs!' % (time.time() - start_time))  
+            predict = model.predict(X_test) 
+            train_out = model.predict(X_train)  
+            
+            tr[classifier] = train_out
+            te[classifier] = predict
+        te["f_name"] = a1
+        te["line_index"] = a2
+        te["freq_index"] = a3
+
+        
+        te.to_csv("eve_test2.csv",index = None)
+        # print("result save done")
+    s2 = timeit.default_timer()  
+    print ('Runing time is (mins):',round((s2 -s1)/60,2))
+    
+
+
+    
+def jiaoji(listA,listB):
+    return list(set(listA).intersection(set(listB)))
+
+
+
+def main():
+    s1 = timeit.default_timer()  
+
+    run()
+    # test_X = datapick2.eve_test() 
+    # print(test_X.shape)
+    s2 = timeit.default_timer()
+    #running time
+    print('Time: ', (s2 - s1)/60 )
+if __name__ == '__main__':  
+
+    main()
+
---
+++ LICENSE.md/classification/2year/IC_B_report/tk17.py
@@ -0,0 +1,122 @@
+import math
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta
+import timeit
+
+path = "time/"
+def add_zero(a):
+    if(len(a)<2):
+        return "0"+a
+    else:
+        return a
+def cc():
+    df= pd.read_csv(path + "2017_test.csv")
+    dt = df.values
+    st = []
+    for i in range(df.shape[0]):    
+        temp= dt[i,0]
+        x= temp.split("_")
+        x1 = x[0].split("=")
+        x2 = x[1].split("=")
+        x3 = x[2].split("=")
+        x4 = x[3].split("=")
+        x5 = x4[1].split(".")
+        
+        # print(x1[1],x2[1],x3[1],x5[0])
+        st.append(x1[1]+"/"+x2[1]+"/2017 "+ x3[1]+":"+add_zero(x5[0]))
+        
+
+    df["time"] = st
+    print(df.head())
+    dt = df.values
+    # df.shape[0]
+    st1 = []
+    st2 = []
+    for i in range(df.shape[0]):
+        temp1= dt[i,1]
+        temp2= dt[i,2]
+        temp1 = -(3600 - temp1 )/60
+        temp2 = -(3600 - temp2 )/60
+        st1.append(temp1)
+        st2.append(temp2)
+        # print(temp1)
+        # print(temp2)
+    df["line_index"] = st1
+    df["freq_index"] = st2
+    
+    df = df.dropna(axis =1,how = "any")
+    df.pop("f_name")
+    print(df.head())
+    
+    df.to_csv("2017_2.csv",index = None)
+
+def change_label():
+    path = "orignal/"
+    df= pd.read_csv(path+"2017B_test.csv")
+    
+    dt1 = df[df["Event"] =="Freq"]
+    dt2 = df[df["Event"] =="Osc"]
+    
+    print(dt1.shape)
+    print(dt2.shape)
+    
+    dt1 = dt1["Start"].values.tolist()
+    dt2 = dt2["Start"].values.tolist()
+    
+    df= pd.read_csv("2017_2.csv")
+    ind1 = df.loc[df.time.isin(dt1)].index
+    ind2 = df.loc[df.time.isin(dt2)].index
+    
+    print(len(ind1), len(ind2))
+    df.RF[ind1] = 2
+    df.RF[ind2] = 3
+    df.to_csv("2017_3.csv",index = None)
+    
+def deal_time():
+    res =[]
+    df= pd.read_csv("2017_3.csv")
+    df['time']=pd.to_datetime(df['time'], format='%m/%d/%Y %H:%M')
+    st =[]
+    for i in range(df.shape[0]):
+        if(df.loc[i,"RF"] == 2):
+            st.append(df.loc[i,"freq_index"])
+        else:
+            st.append(df.loc[i,"line_index"])
+
+    df["ind"] = st
+    
+    for i in range(df.shape[0]):
+        temp = df.loc[i,"time"] +timedelta(seconds = df.loc[i,"ind"])
+        res.append(temp)
+        
+    df["c_time"] = res
+    df['c_time'] = df['c_time'].dt.strftime('%Y/%m/%d %H:%M:%S.%f')
+    print(df.head())
+    
+    st2=[]
+    print(df["RF"].dtypes)
+    for i in range(df.shape[0]):
+        temp = df.loc[i,"RF"] 
+        if(temp == 0):
+            st2.append("Line")
+        elif(temp == 1):
+            st2.append("Transformer")
+        elif(temp == 2):
+            st2.append("Frequency ") 
+        elif(temp == 3):
+            st2.append("Oscillation")   
+    df["event"] = st2
+    df.to_csv("2017_4.csv",index = None)
+def main():
+    cc()
+    change_label()
+    deal_time()
+    
+
+
+
+if __name__ == "__main__":
+    main()
+
+
